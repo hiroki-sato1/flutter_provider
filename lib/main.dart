00cc28a6626aeb98bc5891d2f5c41492d029dfd1
hiroki.sato1 <hiroki.sato1@persol.co.jp>
2020-03-09 17:54:39 +0900

    :sparkles:storeを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider/store/counter_store.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +13,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ChangeNotifierProvider(
+        create: (_) => CounterStore(),
+        child: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
@@ -46,18 +51,41 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            CountText()
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: IncrementButton(),
+    );
+  }
+}
+
+class CountText extends StatelessWidget {
+  const CountText({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    CounterStore _counterStore = Provider.of<CounterStore>(context);
+    int counter = _counterStore.counter;
+    return Text(
+      '$counter',
+      style: Theme.of(context).textTheme.display1,
+    );
+  }
+}
+
+class IncrementButton extends StatelessWidget {
+  const IncrementButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    CounterStore _counterStore =
+        Provider.of<CounterStore>(context, listen: false);
+
+    return FloatingActionButton(
+      onPressed: _counterStore.incrementCounter,
+      tooltip: 'Increment',
+      child: Icon(Icons.add),
     );
   }
 }
